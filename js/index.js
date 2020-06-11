7b6f5b1055c0c82f6822a793bbefa74c1214be64
Galusss <gael.paquien.contact@gmail.com>
2020-06-11 21:10:33 +0200

    index & produit.js

---
+++ js/index.js
@@ -0,0 +1,92 @@
+// Requête GET vers API afin de récupérer les données des produits
+let request = new XMLHttpRequest();
+request.open('GET', "http://localhost:3000/api/cameras");
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var produits = request.response;
+    for(let i = 0; i < produits.length; i++) { // Permet de parcourir toute les données des différents produits et de tout stocké dans une seule variable.
+        let infoProduits = {
+            name: produits[i].name,
+            lenses: produits[i].lenses,
+            description: produits[i].description,
+            price: produits[i].price,
+            imageUrl: produits[i].imageUrl,
+            _id: produits[i]._id
+        }
+        let img = document.createElement("img"); // Création de la variable "img" qui créer un élément image, cette variable est servira pout tout les produits
+        function imgAttribute(){ // Tout les attributs communs des images intégré a la page d'acceuil afin de ne pas les répeter
+            img.setAttribute("width", "350")
+            img.setAttribute("height", "300")
+            img.setAttribute("alt", "Voici l'un de nos modèle de caméra que vous pouvez retrouver sur Oricono'.");
+            img.setAttribute("src", infoProduits.imageUrl);
+        }
+        // Les informations et les images des différents produits s'affichent correctement dans l'ordre sur la page d'accueil selon l'ID du produit
+        if (infoProduits._id === "5be1ed3f1c9d44000030b061") { // Premier produit
+            let name = document.getElementById("card-name-zurss")
+            let lenses = document.getElementById("card-lenses-zurss")
+            let description = document.getElementById("card-description-zurss")
+            let price = document.getElementById("card-price-zurss")
+            let imgParent = document.getElementById("imgParent-card-camera-zurss");
+            name.innerHTML = infoProduits.name;
+            lenses.innerHTML = "Lentilles : " + infoProduits.lenses[0] + " ou " + infoProduits.lenses[1];
+            description.innerHTML = "Description : " + infoProduits.description; 
+            price.innerHTML = "Prix : " + infoProduits.price + " €";
+            imgParent.appendChild(img);
+            img.setAttribute("id", "img-card-camera-zurss")
+            imgAttribute()
+        } else if (infoProduits._id === "5be1ef211c9d44000030b062") { // Second produit
+            let name = document.getElementById("card-name-hirsch")
+            let lenses = document.getElementById("card-lenses-hirsch")
+            let description = document.getElementById("card-description-hirsch")
+            let price = document.getElementById("card-price-hirsch")
+            let imgParent = document.getElementById("imgParent-card-camera-hirsch");
+            name.innerHTML = infoProduits.name;
+            lenses.innerHTML = "Lentilles : " + infoProduits.lenses[0] + " ou " + infoProduits.lenses[1] + " ou " + infoProduits.lenses[2];
+            description.innerHTML = "Description : " + infoProduits.description; 
+            price.innerHTML = "Prix : " + infoProduits.price + " €";
+            imgParent.appendChild(img);
+            img.setAttribute("id", "img-card-camera-hirsch")
+            imgAttribute()
+        } else if (infoProduits._id === "5be9bc241c9d440000a730e7") { // Troisième produit
+            let name = document.getElementById("card-name-franck")
+            let lenses = document.getElementById("card-lenses-franck")
+            let description = document.getElementById("card-description-franck")
+            let price = document.getElementById("card-price-franck")
+            let imgParent = document.getElementById("imgParent-card-camera-franck");
+            name.innerHTML = infoProduits.name;
+            lenses.innerHTML = "Lentilles : " + infoProduits.lenses;
+            description.innerHTML = "Description : " + infoProduits.description;
+            price.innerHTML = "Prix : " + infoProduits.price + " €";
+            imgParent.appendChild(img);
+            img.setAttribute("id", "img-card-camera-franck")
+            imgAttribute()
+        } else if (infoProduits._id === "5be9c4471c9d440000a730e8") { // Quatrième produit
+            let name = document.getElementById("card-name-kuros")
+            let lenses = document.getElementById("card-lenses-kuros")
+            let description = document.getElementById("card-description-kuros")
+            let price = document.getElementById("card-price-kuros")
+            let imgParent = document.getElementById("imgParent-card-camera-kuros");
+            name.innerHTML = infoProduits.name;
+            lenses.innerHTML = "Lentilles : " + infoProduits.lenses[0] + " ou " + infoProduits.lenses[1];
+            description.innerHTML = "Description : " + infoProduits.description;
+            price.innerHTML = "Prix : " + infoProduits.price + " €";
+            imgParent.appendChild(img);
+            img.setAttribute("id", "img-card-camera-kuros")
+            imgAttribute()
+        } else if (infoProduits._id === "5be9c4c71c9d440000a730e9") { // Cinquième produit
+            let name = document.getElementById("card-name-katatone")
+            let lenses = document.getElementById("card-lenses-katatone")
+            let description = document.getElementById("card-description-katatone")
+            let price = document.getElementById("card-price-katatone")
+            let imgParent = document.getElementById("imgParent-card-camera-katatone");
+            name.innerHTML = infoProduits.name;
+            lenses.innerHTML = "Lentilles : " + infoProduits.lenses[0] + " ou " + infoProduits.lenses[1] + " ou " + infoProduits.lenses[2];
+            description.innerHTML = "Description : " + infoProduits.description;
+            price.innerHTML = "Prix : " + infoProduits.price + " €";
+            imgParent.appendChild(img);
+            img.setAttribute("id", "img-card-camera-katatone")
+            imgAttribute()
+        }
+    }
+}
--- js/produit.js
+++ js/produit.js
@@ -1,282 +1,108 @@
-let nom = document.getElementById("nom")
-let marque = document.getElementById("marque")
-let lentilles = document.getElementById("lentilles")
-let description = document.getElementById("description")
-let prix = document.getElementById("prix")
-let lentille1 = document.getElementById("lense1")
-let lentille2 = document.getElementById("lense2")
-let formName = document.getElementById("choice-name")
-let formPrix = document.getElementById("price")
+// Requête GET vers API afin de récupérer les données des produits.
+let request = new XMLHttpRequest();
+request.open('GET', "http://localhost:3000/api/cameras");
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    let produits = request.response; // Stock toute les données des produits dans un tableau de la taille du nombre de produit.
 
+    function infosProduit(){ // Permet de modifier toute les informations du produit.
+        let name = document.getElementById("name");
+        let description = document.getElementById("description");
+        let price = document.getElementById("price");
+        name.innerHTML = produits.name;
+        description.innerHTML = "Description : " + produits.description;
+        price.innerHTML = "Prix : " + produits.price;
+    }
 
+    function imgProduit() { // Permet de créer l'image du produit.
+        let img = document.createElement("img");
+        let imgParent = document.getElementById("imgParent");
+        imgParent.appendChild(img);
+        img.setAttribute("src", produits.imageUrl);
+        img.classList.add("img-customize");
+        img.setAttribute("alt", "Personnalier votre caméra pour valider votre commande chez Oricono.");
+    }
 
+    // Envoie la valeur des lentilles dans la premiere option du formulaire (les autres options s'adaptent plus tard selon le nombre de choix de lentille du produit)
+    function formLense1() {
+        let lense1 = document.getElementById("lense1");
+        lense1.setAttribute("value", produits.lenses[0]);
+        lense1.innerHTML = produits.lenses[0];
+    }
 
-// Création de l'objet concernant l'article 'Fuxi-480'
-let fuxiJson = {
-    nom: "Fuxi-480", 
-    marque: "Fuxi",
-    lentilles: "29mm ou 38mm",
-    lentille1: "29mm",
-    lentille2: "38mm",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    prix: 299
-}
-
-// Fonction permettant d'afficher les informations du produit sélectionner (page dynamique)
-function produitFuxi() {
-    // Stockage de l'objet 'fuxiJson' dans le LocalStorage
-    let fuxi = JSON.stringify(fuxiJson);
-    localStorage.setItem("Article Fuxi", fuxi);
-    // Lecture de l'objet 'fuxiJson' qui est stocker dans le LocalStorage
-    fuxi = localStorage.getItem("Article Fuxi");
-    fuxiJson = JSON.parse(fuxi);
-    // Affiche les informations du produit en les récupérant depuis le LocalStorage
-    nom.innerHTML = fuxiJson.nom
-    marque.innerHTML = "Marque : " + fuxiJson.marque
-    lentilles.innerHTML = "Lentilles : " + fuxiJson.lentilles
-    description.innerHTML = "Description : " + fuxiJson.description
-    prix.innerHTML = "Prix : " + fuxiJson.prix + " €"
-    // Afficher les bonne valeurs pour le formulaire ou les choix ne seront pas visible par l'user, cela permettra de les envoyer dans le localStorage
-    formName.setAttribute("value", "Fuxi-480")
-    formPrix.setAttribute("value", "299")
-    // Afficher les bonnes valeurs pour le formulaire de choix des lentilles
-    lense1.innerHTML = fuxiJson.lentille1
-    lense2.innerHTML = fuxiJson.lentille2
-    lense1.setAttribute("value", "29mm")
-    lense2.setAttribute("value", "38mm")
-    // Création d'une image pour mettre l'image représentant le produit
-    let img = document.createElement("img")
-    let imgParent = document.getElementById("imgParent")
-    imgParent.appendChild(img)
-    img.src = "images/fuxi480.jpg"
-    img.classList.add("img-customize")
-    img.setAttribute("width", "900px")
-    img.setAttribute("height", "500px")
-    img.setAttribute("alt", "Personnalier votre appareil photo Fuxi-480 pour valider votre commande chez Oricono.")
-}
-
-
-
-// Création de l'objet concernant l'article 'Kira-S50'
-let kiraJson = {
-    nom: "Kira-S50",
-    marque: "Kira",
-    lentilles: "49mm ou 37mm",
-    lentille1: "49mm",
-    lentille2: "37mm",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    prix: 809
-}
-
-// Fonction permettant d'afficher les informations du produit sélectionner (page dynamique)
-function produitKira() {
-    // Stockage de l'objet 'kiraJson' dans le LocalStorage
-    let kira = JSON.stringify(kiraJson);
-    localStorage.setItem("Article Kira", kira);
-    // Lecture de l'objet 'kiraJson' qui est stocker dans le LocalStorage
-    kira = localStorage.getItem("Article Kira");
-    kiraJson = JSON.parse(kira);
-    // Affiche les informations du produit en les récupérant depuis le LocalStorage
-    nom.innerHTML = kiraJson.nom
-    marque.innerHTML = "Marque : " + kiraJson.marque
-    lentilles.innerHTML = "Lentilles : " + kiraJson.lentilles
-    description.innerHTML = "Description : " + kiraJson.description
-    prix.innerHTML = "Prix : " + kiraJson.prix + " €"
-    // Afficher les bonne valeurs pour le formulaire ou les choix ne seront pas visible par l'user, cela permettra de les envoyer dans le localStorage
-    formName.setAttribute("value", "Kira-S50")
-    formPrix.setAttribute("value", "809")
-    // Afficher les bonnes valeurs pour le formulaire de choix des lentilles
-    lense1.innerHTML = kiraJson.lentille1
-    lense2.innerHTML = kiraJson.lentille2
-    lense1.setAttribute("value", "49mm")
-    lense2.setAttribute("value", "37mm")
-    // Création d'une image pour mettre l'image représentant le produit
-    let img = document.createElement("img")
-    let imgParent = document.getElementById("imgParent")
-    imgParent.appendChild(img)
-    img.src = "images/kiraS50.jpg"
-    img.classList.add("img-customize")
-    img.setAttribute("width", "900px")
-    img.setAttribute("height", "500px")
-    img.setAttribute("alt", "Personnalier votre appareil photo Kira-S50 pour valider votre commande chez Oricono.")
-}
-
-
-
-// Création de l'objet concernant l'article 'Boki-100T'
-let bokiJson = {
-    nom: "Boki-100T",
-    marque: "Boki",
-    lentilles: "31mm ou 16mm",
-    lentille1: "31mm",
-    lentille2: "16mm",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    prix: 1119
-}
-
-// Fonction permettant d'afficher les informations du produit sélectionner (page dynamique)
-function produitBoki() {
-    // Stockage de l'objet 'bokiJson' dans le LocalStorage
-    let boki = JSON.stringify(bokiJson);
-    localStorage.setItem("Article Boki", boki);
-    // Lecture de l'objet 'bokiJson' qui est stocker dans le LocalStorage
-    boki = localStorage.getItem("Article Boki");
-    bokiJson = JSON.parse(boki);
-    // Affiche les informations du produit en les récupérant depuis le LocalStorage
-    nom.innerHTML = bokiJson.nom
-    marque.innerHTML = "Marque : " + bokiJson.marque
-    lentilles.innerHTML = "Lentilles : " + bokiJson.lentilles
-    description.innerHTML = "Description : " + bokiJson.description
-    prix.innerHTML = "Prix : " + bokiJson.prix + " €"
-    // Afficher les bonne valeurs pour le formulaire ou les choix ne seront pas visible par l'user, cela permettra de les envoyer dans le localStorage
-    formName.setAttribute("value", "Boki-100T")
-    formPrix.setAttribute("value", "1119")
-    // Afficher les bonnes valeurs pour le formulaire de choix des lentilles
-    lense1.innerHTML = bokiJson.lentille1
-    lense2.innerHTML = bokiJson.lentille2
-    lense1.setAttribute("value", "31mm")
-    lense2.setAttribute("value", "16mm")
-    // Création d'une image pour mettre l'image représentant le produit
-    let img = document.createElement("img")
-    let imgParent = document.getElementById("imgParent")
-    imgParent.appendChild(img)
-    img.src = "images/Boki100T.jpg"
-    img.classList.add("img-customize")
-    img.setAttribute("width", "900px")
-    img.setAttribute("height", "500px")
-    img.setAttribute("alt", "Personnalier votre appareil photo Boki-100T pour valider votre commande chez Oricono.")
-}
-
-
-
-// Création de l'objet concernant l'article 'Tuxor'
-let tuxorJson = {
-    nom: "Tuxor",
-    marque: "Tuxor",
-    lentilles: "21mm ou 19mm",
-    lentille1: "21mm",
-    lentille2: "19mm",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    prix: 2098
-}
-
-// Fonction permettant d'afficher les informations du produit sélectionner (page dynamique)
-function produitTuxor() {
-    // Stockage de l'objet 'tuxorJson' dans le LocalStorage
-    let tuxor = JSON.stringify(tuxorJson);
-    localStorage.setItem("Article Tuxor", tuxor);
-    // Lecture de l'objet 'tuxorJson' qui est stocker dans le LocalStorage
-    tuxor = localStorage.getItem("Article Tuxor");
-    tuxorJson = JSON.parse(tuxor);
-    // Affiche les informations du produit en les récupérant depuis le LocalStorage
-    nom.innerHTML = tuxorJson.nom
-    marque.innerHTML = "Marque : " + tuxorJson.marque
-    lentilles.innerHTML = "Lentilles : " + tuxorJson.lentilles
-    description.innerHTML = "Description : " + tuxorJson.description
-    prix.innerHTML = "Prix : " + tuxorJson.prix + " €"
-    // Afficher les bonne valeurs pour le formulaire ou les choix ne seront pas visible par l'user, cela permettra de les envoyer dans le localStorage
-    formName.setAttribute("value", "Tuxor")
-    formPrix.setAttribute("value", "2098")
-    // Afficher les bonnes valeurs pour le formulaire de choix des lentilles
-    lense1.innerHTML = tuxorJson.lentille1
-    lense2.innerHTML = tuxorJson.lentille2
-    lense1.setAttribute("value", "21mm")
-    lense2.setAttribute("value", "19mm")
-    // Création d'une image pour mettre l'image représentant le produit
-    let img = document.createElement("img")
-    let imgParent = document.getElementById("imgParent")
-    imgParent.appendChild(img)
-    img.src = "images/tuxor.jpg"
-    img.classList.add("img-customize")
-    img.setAttribute("width", "900px")
-    img.setAttribute("height", "500px")
-    img.setAttribute("alt", "Personnalier votre appareil photo Tuxor pour valider votre commande chez Oricono.")
-}
-
-
-
-// Création de l'objet concernant l'article 'Piker'
-let pikerJson = {
-    nom: "Piker",
-    marque: "Piker",
-    lentilles: "19mm ou 29mm",
-    lentille1: "19mm",
-    lentille2: "29mm",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    prix: 2479
-}
-
-// Fonction permettant d'afficher les informations du produit sélectionner (page dynamique)
-function produitPiker() {
-    // Stockage de l'objet 'tuxorJson' dans le LocalStorage
-    let piker = JSON.stringify(pikerJson);
-    localStorage.setItem("Article Piker", piker);
-    // Lecture de l'objet 'tuxorJson' qui est stocker dans le LocalStorage
-    piker = localStorage.getItem("Article Piker");
-    pikerJson = JSON.parse(piker);
-    // Affiche les informations du produit en les récupérant depuis le LocalStorage
-    nom.innerHTML = pikerJson.nom
-    marque.innerHTML = "Marque : " + pikerJson.marque
-    lentilles.innerHTML = "Lentilles : " + pikerJson.lentilles
-    description.innerHTML = "Description : " + pikerJson.description
-    prix.innerHTML = "Prix : " + pikerJson.prix + " €"
-    // Afficher les bonne valeurs pour le formulaire ou les choix ne seront pas visible par l'user, cela permettra de les envoyer dans le localStorage
-    formName.setAttribute("value", "Piker")
-    formPrix.setAttribute("value", "2479")
-    // Afficher les bonnes valeurs pour le formulaire de choix des lentilles
-    lense1.innerHTML = pikerJson.lentille1
-    lense2.innerHTML = pikerJson.lentille2
-    lense1.setAttribute("value", "19mm")
-    lense2.setAttribute("value", "29mm")
-    // Création d'une image pour mettre l'image représentant le produit
-    let img = document.createElement("img")
-    let imgParent = document.getElementById("imgParent")
-    imgParent.appendChild(img)
-    img.src = "images/piker.jpg"
-    img.classList.add("img-customize")
-    img.setAttribute("width", "900px")
-    img.setAttribute("height", "500px")
-    img.setAttribute("alt", "Personnalier votre appareil photo Piker pour valider votre commande chez Oricono.")
-}
-
-
-
-// Appel de la bonne fonction selon l'URL détecté par window.location (1 URL différent pour chaque produit)
-function AfficheMonProduit() {
-    let Adress = window.location;
-    if (Adress ==  /* GITHUB ->*/ "https://galusss.github.io/Formation_projet5/produit.html#Fuxi" /*LOCAL -> "file:///D:/GitHub/Formation_projet5/produit.html#Fuxi"*/) {
-        produitFuxi()
-    } else if (Adress ==  /* GITHUB ->*/ "https://galusss.github.io/Formation_projet5/produit.html#Kira" /*LOCAL -> "file:///D:/GitHub/Formation_projet5/produit.html#Kira"*/) {
-        produitKira()
-    } else if (Adress ==  /* GITHUB ->*/ "https://galusss.github.io/Formation_projet5/produit.html#Boki" /*LOCAL -> "file:///D:/GitHub/Formation_projet5/produit.html#Boki"*/) {
-        produitBoki()
-    } else if (Adress ==  /* GITHUB ->*/ "https://galusss.github.io/Formation_projet5/produit.html#Tuxor" /*LOCAL -> "file:///D:/GitHub/Formation_projet5/produit.html#Tuxor"*/) {
-        produitTuxor()
-    } else if (Adress ==  /* GITHUB ->*/ "https://galusss.github.io/Formation_projet5/produit.html#Piker" /*LOCAL -> "file:///D:/GitHub/Formation_projet5/produit.html#Piker"*/) {
-        produitPiker()
+    // Envoie les valeurs du produit dans le formulaire (ces infos ne sont pas visible pas l'utilisateur, cela permet d'envoyer des données vers la page panier.html)
+    function formInfo() {
+        let formName = document.getElementById("formName");
+        let formPrice = document.getElementById("formPrice");
+        let formId = document.getElementById("formId");
+        formName.setAttribute("value", produits.name);
+        formPrice.setAttribute("value", produits.price);
+        formId.setAttribute("value", produits._id);
+    }
+    
+    // Affiche les bonne informations celon le paramètre "hash" de l'URL ; Des options pour le choix de lentille sont également ajouter dans le formulaire celon le produit
+    let formLenses = document.getElementById("formLenses");
+    if (window.location.hash === "#5be1ed3f1c9d44000030b061") { // Premier produit
+        produits = produits[0];
+        lenses.innerHTML = "Lentilles : " + produits.lenses[0] + " ou " + produits.lenses[1];
+        let lense2 = document.createElement("option");
+        formLenses.appendChild(lense2);
+        lense2.setAttribute("value", produits.lenses[1]);
+        lense2.innerHTML = produits.lenses[1];
+        imgProduit()
+        infosProduit()
+        formLense1()
+        formInfo()
+    } else if (window.location.hash === "#5be1ef211c9d44000030b062") { // Second produit
+        produits = produits[1];
+        lenses.innerHTML = "Lentilles : " + produits.lenses[0] + " ou " + produits.lenses[1] + " ou " + produits.lenses[2];
+        let lense2 = document.createElement("option");
+        formLenses.appendChild(lense2);
+        lense2.setAttribute("value", produits.lenses[1]);
+        lense2.innerHTML = produits.lenses[1];
+        let lense3 = document.createElement("option");
+        formLenses.appendChild(lense3);
+        lense3.setAttribute("value", produits.lenses[2]);
+        lense3.innerHTML = produits.lenses[2];
+        imgProduit()
+        infosProduit()
+        formLense1()
+        formInfo()
+    } else if (window.location.hash === "#5be9bc241c9d440000a730e7") { // Troisième produit
+        produits = produits[2];
+        lenses.innerHTML = "Lentilles : " + produits.lenses[0];
+        imgProduit()
+        infosProduit()
+        formLense1()
+        formInfo()
+    } else if (window.location.hash === "#5be9c4471c9d440000a730e8") { // Quatrième produit
+        produits = produits[3];
+        lenses.innerHTML = "Lentilles : " + produits.lenses[0] + " ou " + produits.lenses[1];
+        let lense2 = document.createElement("option");
+        formLenses.appendChild(lense2);
+        lense2.setAttribute("value", produits.lenses[1]);
+        lense2.innerHTML = produits.lenses[1];
+        imgProduit()
+        infosProduit()
+        formLense1()
+        formInfo()
+    } else if (window.location.hash === "#5be9c4c71c9d440000a730e9") { // Cinquième produit
+        produits = produits[4];
+        lenses.innerHTML = "Lentilles : " + produits.lenses[0] + " ou " + produits.lenses[1] + " ou " + produits.lenses[2];
+        let lense2 = document.createElement("option");
+        formLenses.appendChild(lense2);
+        lense2.setAttribute("value", produits.lenses[1]);
+        lense2.innerHTML = produits.lenses[1];
+        let lense3 = document.createElement("option");
+        formLenses.appendChild(lense3);
+        lense3.setAttribute("value", produits.lenses[2]);
+        lense3.innerHTML = produits.lenses[2];
+        imgProduit()
+        infosProduit()
+        formLense1()
+        formInfo()
     }
 }
-AfficheMonProduit() // Appel de la fonction qui entrainera l'appel de la bonne fonction selon le produit choisis
 
 
-
-
-// Lors du click sur le bouton du formulaire la fonction "produitSelectionne" est appliquer
-let btn = document.getElementById("btn_produit")
-btn.addEventListener("click", produitSelectionner)
-
-// Fonction permetant d'enregistrer les données du formulaire dans le localStorage, cela va permettre de pouvoir afficher le produit séléctionner dans le récap de commande. 
-function produitSelectionner(){
-    let produitSelectJson = {
-        nom: document.getElementById("choice-name").value,
-        prix:document.getElementById("price").value,
-        lentilles:document.getElementById("lenses").value,
-    }
-    let produitSelect = JSON.stringify(produitSelectJson);
-    localStorage.setItem("Produit Sélectionné", produitSelect);
-    // Si l'utilisateur choisis un nouveau produits celui si sera stocké dans un nouveau objet depuis le localStorage
-    var produits = "Produit Séléctionné";
-    var produitstID = localStorage.length + 1;
-    localStorage[produits + produitstID] = produitSelect;
-
-}
